13d93b539bea2b690a50a379f65e1445693362d6
Craig Pirie <craigstevenpirie@gmail.com>
2023-02-06 15:24:42 +0000

    CBR

---
+++ CaseBasedReasoning.py
@@ -0,0 +1,38 @@
+#Contain the case-base within a class
+class CBR:
+  #Initialise the case-base data and k-NN model
+  def __init__(self, categorical, neighbours, filepath='', raw_data=[], inputDict={}):
+    self.data = self.load_data(categorical, filepath, raw_data)
+    self.model = self.fit_model(neighbours)
+    self.inputDict = inputDict
+
+  #Fit the k-NN case-base
+  def fit_model(self, neighbours):
+    knn = NearestNeighbors(n_neighbors=len(self.data))
+    knn.fit(self.data)
+
+    return knn
+
+  #Read the data from the CSV file into a Pandas dataframe
+  def load_data(self, categorical, filepath='', raw_data=[]):
+    if(len(raw_data) > 0):
+      return raw_data
+    else: 
+      data = pd.read_csv(filepath)
+    
+    #One-hot encode all categorical data as k-NN is numeric
+    ohe = OneHotEncoder()
+    transformed = ohe.fit_transform(data[categorical])
+
+    return transformed
+
+  #Retrieve the neighbours for a query along with its distance
+  def retrieve(self, index, num_neighbours):
+    NEIGHBOURS_AND_DISTANCES = self.model.kneighbors(index.reshape(1,-1), n_neighbors=num_neighbours, return_distance=True)
+    DISTANCES = NEIGHBOURS_AND_DISTANCES[0]
+    NEIGHBOURS = NEIGHBOURS_AND_DISTANCES[1]
+
+    #Convert the neighbour-distances 2-D array into a 1D array of tuples
+    # e.g (neighbour, distance)
+    TRANSPOSED = np.array((NEIGHBOURS,DISTANCES)).T
+    return TRANSPOSED
