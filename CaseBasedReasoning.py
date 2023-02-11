cbdf391032a6c33e2113ebe70cf6611d321c086e
Craig Pirie <craigstevenpirie@gmail.com>
2023-02-11 14:14:17 +0000

    Added limit to retrieval step to speed up calculation

--- CaseBasedReasoning.py
+++ CaseBasedReasoning.py
@@ -6,10 +6,11 @@ import numpy as np
 #Contain the case-base within a class
 class CBR:
   #Initialise the case-base data and k-NN model
-  def __init__(self, categorical, neighbours, filepath='', raw_data=[], inputDict={}):
+  def __init__(self, categorical, neighbours, filepath='', raw_data=[], inputDict={}, limit=1):
     self.data = self.load_data(categorical, filepath, raw_data)
     self.model = self.fit_model(neighbours)
     self.inputDict = inputDict
+    self.limit = limit
 
   #Fit the k-NN case-base
   def fit_model(self, neighbours):
@@ -33,7 +34,7 @@ class CBR:
 
   #Retrieve the neighbours for a query along with its distance
   def retrieve(self, index, num_neighbours):
-    NEIGHBOURS_AND_DISTANCES = self.model.kneighbors(index.reshape(1,-1), n_neighbors=num_neighbours, return_distance=True)
+    NEIGHBOURS_AND_DISTANCES = self.model.kneighbors(index.reshape(1,-1), n_neighbors=round(num_neighbours * self.limit), return_distance=True)
     DISTANCES = NEIGHBOURS_AND_DISTANCES[0]
     NEIGHBOURS = NEIGHBOURS_AND_DISTANCES[1]
 
